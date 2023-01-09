a298c47afcfc4f38ddc0e416892da9969018e964
prashantchhatri <prashantchhatri2022@gmail.com>
2023-01-10 00:10:02 +0530

    maulal oath Signed-off-by: prashantchhatri <prashantchhatri2022@gmail.com>

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,25 @@
 const User = require('../models/user')
 
+module.exports.profile= function(req, res){
+    // console.log(req.cookies);
+    if (req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(err){console.log('error in finding user in signing up'); return;}
+            if(user){
+                return res.render('home', {
+                    title: 'User Profile',
+                    user: 'user'
+                })
+            }
+            return res.redirect('users/sign-in');
+
+            
+        });
+    }else{
+        return res.redirect('users/sign-in');
+    }
+}
+
 module.exports.signIn = function(req, res){
     return res.render('user_sign_in', {
         title: 'Social | SignIn'
@@ -25,7 +45,7 @@ module.exports.create = function(req, res){
         if(!user){
             User.create(req.body, function(err, user){
                 if(err){console.log('error in creating user signing up'); return;}
-                return res.redirect('users/sign-in')
+                return res.redirect('sign-in')
             })
         }else{
             return res.redirect('back');
@@ -35,6 +55,17 @@ module.exports.create = function(req, res){
 }
 
 //sign in and create user session
-module.exports.createSession = function(res, req){
-    //later
+module.exports.createSession = function(req, res){
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return;}
+        if(user){
+            if (user.password != req.body.password){
+            return res.redirect('back');
+            }
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 const userController = require('../controllers/user_controller')
 
-// router.get('/profile', userController.profile);
+router.get('/profile', userController.profile);
 
 
 router.get('/sign-up', userController.signUp);
 router.post('/create', userController.create);
 router.get('/sign-in', userController.signIn);
+router.post('/create-session', userController.createSession);
 
 
 module.exports = router;
